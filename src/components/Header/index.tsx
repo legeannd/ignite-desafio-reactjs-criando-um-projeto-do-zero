164dde07e6cc9d53c50711bd5223457a3f7dd695
Gean Lucas <gean.lfpsa@gmail.com>
2021-05-04 22:10:53 -0300

    Adjusting post banner size

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link';
 import { ReactElement } from 'react';
 import styles from './header.module.scss';
 
 export default function Header(): ReactElement {
   return (
     <div className={styles.container}>
-      <img src="logo.png" alt="Spacetraveler" />
+      <Link href="/">
+        <a>
+          <img src="/logo.png" alt="Spacetraveler" />
+        </a>
+      </Link>
     </div>
   );
 }
